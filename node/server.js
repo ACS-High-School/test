8951a9d34196b09a742380e9407424562f9f9f23
hjp1016 <94664341+hjp1016@users.noreply.github.com>
2024-01-22 15:24:37 +0900

    Update server.js

--- node/server.js
+++ node/server.js
@@ -1,1 +1,14 @@
-d
+const http = require('http');
+
+const hostname = '0.0.0.0'; // 모든 네트워크 인터페이스에서 수신 대기합니다.
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
